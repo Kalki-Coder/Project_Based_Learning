9a90a0387df64b8186f5a395babd0a50c71ec6c5
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2026-01-06 22:54:43 +0530

    progress

---
+++ day_8/practice_ans.js
@@ -0,0 +1,13 @@
+/*This code will
+be ignored by JavaScript
+and will NOT be executed*/ // multiline comment
+
+//JavaScript was invented by Brendan Eich in 1995
+
+//random practice on the go hihi...
+// x = typeof('John' + 35);
+// console.log(x);
+
+// x = typeof(['apple','banana']);
+// console.log(x); Object
+

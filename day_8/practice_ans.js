c63d77987ac363baa26fde29570712f5fa126f78
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2026-01-07 17:49:37 +0530

    progress

--- day_8/practice_ans.js
+++ day_8/practice_ans.js
@@ -11,3 +11,31 @@ and will NOT be executed*/ // multiline comment
 // x = typeof(['apple','banana']);
 // console.log(x); Object
 
+// let x = 'Having fun?';
+// console.log(x.slice(7,10));
+// console.log(x.substring(7,10));
+// console.log(x.substr(7,3));
+
+// let txt = "Hello World";
+// txt = txt.replace("Hello", "Welcome");
+
+// let x = 10;
+// let y = 20;
+// let z = 'The prize is ' + x + y; // The prize is 1020
+// What will be the result of z?
+
+// let x = 2;
+// let z = x.toFixed(10);
+// console.log(z)
+
+// let x = 'Tobias';
+// let z = Number(x);
+// console.log(z);
+
+// let a = Number.POSITIVE_INFINITY;
+// console.log(a);
+
+// let b = Number.NEGATIVE_INFINITY;
+// console.log(b);
+
+// The reverse() method returns a new array, the original array is not affected. false

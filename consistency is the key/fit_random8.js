eea0d7ac0f0d18b9e11977340f1b22113c9fb1db
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2026-01-12 21:40:19 +0530

    progress

--- consistency is the key/fit_random8.js
+++ consistency is the key/fit_random8.js
@@ -1,12 +1,12 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
-console.log(fruits.copyWithin(1, 0));
-console.log(fruits.copyWithin(1, 0, 4));
-fruits.copyWithin(2, 0, 1)
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// console.log(fruits.copyWithin(1, 0));
+// console.log(fruits.copyWithin(1, 0, 4));
+// fruits.copyWithin(2, 0, 1)
 
-const letters = new Set(["a","b","c"]);
+// const letters = new Set(["a","b","c"]);
 
-// List all Elements
-let text = "";
-for (const x of letters.keys()) {
-  text += x;
-}
+// // List all Elements
+// let text = "";
+// for (const x of letters.keys()) {
+//   text += x;
+// }

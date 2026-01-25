fc7d5b97178a3d13e88fde268589d74d4d5c49c6
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2026-01-26 01:58:08 +0530

    delete feature added

---
+++ The To-Do List/script.js
@@ -0,0 +1,24 @@
+const user_input = document.querySelector(".task-input");
+const add_button = document.querySelector(".addbtn");
+const list = document.querySelector(".task-list");
+
+add_button.addEventListener("click", function () {
+  const taskvalue = user_input.value.trim();
+
+  if (taskvalue != "") {
+    const newtask = document.createElement("li");
+    const dlt_button = document.createElement("button");
+    dlt_button.textContent = "X";
+
+    newtask.textContent = taskvalue;
+    newtask.appendChild(dlt_button);
+    list.appendChild(newtask);
+    user_input.value = "";
+
+    dlt_button.addEventListener("click", function () {
+        list.removeChild(newtask);
+    });
+  } else {
+    alert("please enter the task");
+  }
+});

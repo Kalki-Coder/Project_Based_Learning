766c67c10f64483d6ea7931ff4d71487355a1afe
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2026-01-26 22:29:13 +0530

    logic part half made

--- The To-Do List/script.js
+++ The To-Do List/script.js
@@ -2,6 +2,22 @@ const user_input = document.querySelector(".task-input");
 const add_button = document.querySelector(".addbtn");
 const list = document.querySelector(".task-list");
 
+
+// Logic Part
+
+const list_data = function () {
+  const tasks = [];
+  const entered_task = {
+    id: Date.now(),
+    task: user_input.value.trim(),
+    delete: "X"
+  };
+  tasks.push(entered_task);
+}
+
+
+
+
 add_button.addEventListener("click", function () {
   const taskvalue = user_input.value.trim();
 

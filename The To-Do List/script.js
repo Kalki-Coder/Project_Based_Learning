4d24210cfad7976a1a9b414664e7523ee42cd2de
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2026-01-27 01:59:50 +0530

    state driven to-do list with the create, read and delete feature (next will be local storage and update)

--- The To-Do List/script.js
+++ The To-Do List/script.js
@@ -2,39 +2,62 @@ const user_input = document.querySelector(".task-input");
 const add_button = document.querySelector(".addbtn");
 const list = document.querySelector(".task-list");
 
+// --- 1. THE STATE (Data) ---
+// This lives outside so it remembers everything.
+let tasks = [];
 
-// Logic Part
-
-const list_data = function () {
-  const tasks = [];
-  const entered_task = {
-    id: Date.now(),
-    task: user_input.value.trim(),
-    delete: "X"
-  };
-  tasks.push(entered_task);
+// --- 2. THE LOGIC (Modify Data) ---
+function addTask() {
+  const user_text = user_input.value.trim();
+  if (user_text != "") {
+    const newTaskObj = {
+      id: Date.now(),
+      text: user_text,
+    };
+    // Push to our Database (Array)
+    tasks.push(newTaskObj);
+    // Clear the input for new entry
+    user_input.value = "";
+    // Update the screen now!
+    renderTask();
+  } else {
+    alert("task input is not defined");
+  }
 }
 
+// UI (Reflect Data)
+// this function wipes the list and rebuilds it based on the array
+function renderTask() {
+  // first clear the current HTML list (so we don't get the dublicates)
+  list.innerHTML = "";
+  // loop over each item of the saved data array
+  tasks.forEach(function (task) {
+    const li = document.createElement("li");
+    li.innerHTML = `<span>${task.text}</span>
+    <button class="dlt_btn" data-id=${task.id}>X</button>`;
+    list.appendChild(li);
+  });
+  attchscanner();
+}
 
+function attchscanner() {
+  const dlt_btn = document.querySelectorAll(".dlt_btn");
 
-
-add_button.addEventListener("click", function () {
-  const taskvalue = user_input.value.trim();
-
-  if (taskvalue != "") {
-    const newtask = document.createElement("li");
-    const dlt_button = document.createElement("button");
-    dlt_button.textContent = "X";
-
-    newtask.textContent = taskvalue;
-    newtask.appendChild(dlt_button);
-    list.appendChild(newtask);
-    user_input.value = "";
-
-    dlt_button.addEventListener("click", function () {
-        list.removeChild(newtask);
+  dlt_btn.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      const idToDelete = Number(event.target.dataset.id);
+      deleteTask(idToDelete);
     });
-  } else {
-    alert("please enter the task");
-  }
-});
+  });
+}
+
+function deleteTask(id) {
+  tasks = tasks.filter((task) => {
+    return task.id !== id;
+  });
+  renderTask();
+}
+
+renderTask();
+
+add_button.addEventListener("click", addTask);

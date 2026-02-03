955154cbac5cd5e0f37d159fff1d0e51a5f9b387
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2026-02-03 23:43:28 +0530

    variables, data types, data type conversion

---
+++ Part - I/excercise_13.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class excercise_13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        double value = Double.valueOf(scanner.nextLine());
+        System.out.println("You gave the number " + value);
+    }
+}

d8cc15174e30ac4719ab153440ef2739685f4ba1
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2026-02-04 21:57:08 +0530

    int, double, division

--- Part - I/excercise_22.java
+++ Part - I/excercise_22.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class excercise_18 {
+public class excercise_22 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give the first number:");

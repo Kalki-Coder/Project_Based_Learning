2f1a7428fbc2d86a6084fcddb3f7236af7a8a1ac
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2026-02-04 21:55:41 +0530

    int, double, division

---
+++ Part - I/excerise_22.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class excercise_18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        double first = Double.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        double second = Double.valueOf(scanner.nextLine());
+        System.out.println("Give the third number:");
+        double third = Double.valueOf(scanner.nextLine());
+        System.out.println("The average is " + (( 1.0 * first + second + third) / 3));
+    }
+}

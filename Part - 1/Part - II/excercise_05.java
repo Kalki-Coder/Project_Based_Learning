d7be97d84588e625d3d5ab6a27fd24ad7b0bd6bb
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2026-02-06 01:07:44 +0530

    while loop

---
+++ Part - 1/Part - II/excercise_05.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class excercise_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Shall we carry on?");
+            String input = scanner.nextLine();
+            if (input.equals("no")) {
+                break;
+            }
+        }
+    }
+}
